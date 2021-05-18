e486cd66e0811cc14ef646ee2cff119f0c28b82a
mikershov94 <ershovme@gmail.com>
2021-05-18 16:07:49 +0800

    Installed and configed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
+let isProduction = (process.env.NODE_ENV == 'production');
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path      = require('path');
 const webpack   = require('webpack');
 
 module.exports = {
+    
+
     entry: {
         app: [
             './src/index.js'
@@ -10,6 +17,94 @@ module.exports = {
     output: {
         filename: 'js/app.js',
         path: path.resolve(__dirname, 'build'),
-        publicPath: 'build/'
-    }
+        publicPath: ''
+    },
+
+    devServer: {
+        overlay: true,
+        stats: 'errors-only',
+        contentBase: './public',
+        port: 3000
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.s(a|c)ss$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: { sourceMap: true }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: { sourceMap: true }
+                    }
+                ]
+            },
+
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'css-loader',
+                        options: { sourceMap: true }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: { sourceMap: true }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: { sourceMap: true }
+                    },
+                ]
+            },
+
+            {
+                test: /\.(js|jsx)$/,
+                loader: 'babel-loader',
+                exclude: '/node-modules/'
+            },
+
+            {
+                test: /\.(png|gif|jpe?g)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: { name: '[path][name].[ext]' }
+                    },
+                    {
+                        loader: 'img-loader'
+                    },
+                ]
+            },
+
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: { name: '[path][name].[ext]' }
+                    }
+                ]
+            }
+        ],
+    },
+
+    plugins: [
+        new MiniCssExtractPlugin({ filename: './css/main.css' }),
+        new HtmlWebpackPlugin({
+            hash: false,
+            template: './public/index.html',
+            filename: 'index.html'
+        }),
+        new CleanWebpackPlugin(),
+    ]
 }
