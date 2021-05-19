d240ae10b964e1de62ffde2620da814699a27941
mikershov94 <ershovme@gmail.com>
2021-05-19 13:34:12 +0800

    Fixed import app

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './components/app/app';
+import App from './components/app';
 
 ReactDOM.render(React.createElement(App, null, null), document.getElementById('root'));
