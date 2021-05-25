69e6f2e3c8979b76ae1b0a185414084b80ed2534
mikershov94 <ershovme@gmail.com>
2021-05-25 19:28:36 +0800

    Created component - timer

--- src/components/timer-page/timer-page.tsx
+++ src/components/timer-page/timer-page.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import './timer-page.sass';
 
 const TimerPage = () => {
-    
+    return(
+        <div className="timer-page">
+            
+        </div>
+    );
 };
 
-export default TimerPage
+export default TimerPage;
---
+++ src/components/timer/timer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './timer.sass';
+
+const Timer = () => {
+    return(
+        <div className="timer">00:00:00</div>
+    );
+};
+
+export default Timer;
