29074db415d73f402a7253f56b76206e1ec13481
mikershov94 <ershovme@gmail.com>
2021-05-26 12:26:12 +0800

    Fixed bugs

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './app.sass';
 
 import CommentsPage from '../comments-page';
@@ -7,15 +7,28 @@ import { SwipeEventData, useSwipeable } from 'react-swipeable';
 
 const App = ( ) => { 
 
+    const [page, setPage] = useState('comment-page');
     const handlers = useSwipeable({
         onSwipedLeft: (e: SwipeEventData) => {
-            console.log('свайп')
+            if (page == 'comment-page') {
+                setPage('timer-page');
+            }
+        },
+
+        onSwipedRight: (e: SwipeEventData) => {
+            if (page == 'timer-page') {
+                setPage('comment-page');
+            }
         }
     })
 
+    
+    let pageComponent = <CommentsPage />
+    if (page == 'timer-page') pageComponent = <TimerPage />  
+
     return(
         <div className="app-container" {...handlers} >
-            <CommentsPage />
+            {pageComponent}
         </div>  
     );
 }
